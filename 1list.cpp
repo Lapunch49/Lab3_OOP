3bc40ff67d5fbf3f110b04695b606f3c91c73d38
Lapunch49 <bulatovach@gmail.com>
2021-11-24 19:26:48 +0300

    начало

---
+++ 1list.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Storable {
+public:
+	~Storable() {}
+
+
+};
+
+class Storage {
+
+};
+
+
+
+int main() {
+	return 0;
+}
